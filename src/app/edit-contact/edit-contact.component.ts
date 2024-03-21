a142d85767a2add386217303de8513efcb9cf240
Moises T <47845847+mtaira94@users.noreply.github.com>
2024-03-21 15:00:49 -0700

    Finished module 4

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormsModule, NgForm } from '@angular/forms';
-import { Contact } from '../contacts/contact.model';
+import { Contact, addressTypeValues, phoneTypeValues } from '../contacts/contact.model';
 import { ContactsService } from '../contacts/contacts.service';
 import { Subscription } from 'rxjs';
 
@@ -15,9 +15,10 @@ import { Subscription } from 'rxjs';
 export class EditContactComponent implements OnInit, OnDestroy {
   contact: Contact = {
     id: '',
+    personal: false,
     firstName: '',
     lastName: '',
-    dateOfBirth: null,
+    dateOfBirth: '',
     favoritesRanking: 0,
     phone: {
       phoneNumber: '',
@@ -30,7 +31,10 @@ export class EditContactComponent implements OnInit, OnDestroy {
       postalCode: '',
       addressType: '',
     },
+    notes: ''
   };
+  phoneTypes = phoneTypeValues;
+  addressTypes = addressTypeValues;
   contactSub!: Subscription;
 
   constructor(
@@ -55,6 +59,8 @@ export class EditContactComponent implements OnInit, OnDestroy {
 
   saveContact(form: NgForm) {
     console.log(form.value);
+    console.log(this.contact.personal, typeof this.contact.personal);
+
     this.contactsService.saveContact(form.value).subscribe({
       next: () => this.router.navigate(['/contacts'])
     });
