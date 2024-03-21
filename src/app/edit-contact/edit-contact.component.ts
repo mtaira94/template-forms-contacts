907fa6d2a58a32d4f8363d10a8a31d058a105411
Moises T <47845847+mtaira94@users.noreply.github.com>
2024-03-21 13:12:28 -0700

    Removed stuff destroying vs code and binded edit-contact view

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,15 +1,40 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Contact } from '../contacts/contact.model';
+import { ContactsService } from '../contacts/contacts.service';
 
 @Component({
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   standalone: true,
   templateUrl: './edit-contact.component.html',
   styleUrls: ['./edit-contact.component.css']
 })
 export class EditContactComponent implements OnInit {
-  constructor(private route: ActivatedRoute) { }
+  contact: Contact = {
+    id: '',
+    firstName: '',
+    lastName: '',
+    dateOfBirth: null,
+    favoritesRanking: 0,
+    phone: {
+      phoneNumber: '',
+      phoneType: '',
+    },
+    address: {
+      streetAddress: '',
+      city: '',
+      state: '',
+      postalCode: '',
+      addressType: '',
+    },
+  };
+
+  constructor(
+    private route: ActivatedRoute,
+    private contactsService: ContactsService
+  ) { }
 
   ngOnInit() {
     const contactId = this.route.snapshot.params['id'];
@@ -17,6 +42,6 @@ export class EditContactComponent implements OnInit {
   }
 
   saveContact() {
-
+    console.log(this.contact);
   }
 }
